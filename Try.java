f64839d2d0a339d45ffd77f41bad0468915cf372
unknown <>
2015-10-30 21:19:27 +0800

    try

---
+++ Try.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.animal.Lesson3;
+
+/**
+ *
+ * @author asus1
+ */
+public class Try {
+    public static void main(String[] args){
+    Dog dog=new Dog();
+    dog.age=10;
+    dog.name="旺财";
+    dog.eat();
+    }
+    
+}
