060738b003a369a10caf1cd82e0dbd673788f643
unknown <>
2015-10-30 20:59:20 +0800

    first

---
+++ Animal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.animal.Lesson3;
+
+/**
+ *
+ * @author asus1
+ */
+public class Animal {
+    public int age;
+    public String name;
+    public void eat(){
+        System.out.println("名字："+name+"  年龄;"+age+"  动物具有吃东西的能力");
+    }
+    
+            
+    
+}
