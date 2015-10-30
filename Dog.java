72fbbe679b62416bf9beaeb6571d7015c9262d59
unknown <>
2015-10-30 21:14:46 +0800

    Dog

---
+++ Dog.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.animal.Lesson3;
+
+/**
+ *
+ * @author asus1
+ */
+public class Dog extends Animal {
+   public void eat(){
+        System.out.println("名字："+name+"  年龄;"+age+"  狗爱吃肉和骨头");
+     }
+    
+}
